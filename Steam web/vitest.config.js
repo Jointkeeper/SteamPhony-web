0d7f1a038d45e34b2700e351409db57b10a71e18
Jointkeeper <julianosobramko@gmail.com>
2025-07-07 13:36:56 +0700

    chore(test): enable globals option in vitest

--- Steam web/vitest.config.js
+++ Steam web/vitest.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     setupFiles: './src/test/setupTests.js',
     exclude: ['e2e/**', 'src/test/e2e/**'],
     include: ['src/**/*.test.{js,jsx,ts,tsx}'],
+    globals: true,
   },
   resolve: {
     alias: {
